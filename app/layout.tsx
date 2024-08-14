817c394fbbfe009b0b971049d023e879f2bccabf
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2024-08-14 12:27:38 +0530

    new theme
    Signed-off-by: Abhishek kushwaha <abhishekkushwaha1479@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn(
-        "min-h-screen bg-background font-sans antialiased mx-auto",
+        "min-h-screen font-sans antialiased mx-auto",
         inter.variable,
         schoolbell.variable,
         inter.className,
